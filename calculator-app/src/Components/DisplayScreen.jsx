16a9a5b5331e8f756a98772de6e6b3759cb53f85
adhithyaa <adhthyaa.mv@kalvium.community>
2024-01-27 17:59:09 +0530

    done

--- calculator-app/src/Components/DisplayScreen.jsx
+++ calculator-app/src/Components/DisplayScreen.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Button from "./Button";
+import './../App.css'
 
 function DisplayScreen() {
     const [Btnvalue, setBtnValue] = useState("");
@@ -18,7 +19,7 @@ function DisplayScreen() {
         setResult("")
     }
 
-    function calculate(){
+    function calculate() {
         try {
             setResult(eval(Btnvalue))
         } catch (error) {
@@ -28,43 +29,46 @@ function DisplayScreen() {
 
     return (
         <>
-        <h1>CALCULATOR</h1>
-        <div className="root">
-            <div className="parent">
-                <input
-                type="text"
-                value={Btnvalue.toString()}
-                readOnly
-                />
-                <input type="text" placeholder="0" value={result} readOnly />
-        </div>
-        <div className="buttonss">
-        <Button value={"7"} onClick={() => takeInput("7")} />
-        <Button value={"8"} onClick={() => takeInput("8")} />
-        <Button value={"9"} onClick={() => takeInput("9")} />
-        <Button value={"/"} onClick={() => takeInput("/")} />
 
-        <Button value={"4"} onClick={() => takeInput("4")} />
-        <Button value={"5"} onClick={() => takeInput("5")} />
-        <Button value={"6"} onClick={() => takeInput("6")} />
-        <Button value={"*"} onClick={() => takeInput("*")} />
+            <div className="root">
+                <div className="parent">
+                    <input
+                        type="text"
+                        value={Btnvalue.toString()}
+                        readOnly
+                    />
+                    <input type="text" placeholder="0" value={result} readOnly />
+                </div>
+                <div className="buttonss">
+                    <Button value={"1"} onClick={() => takeInput("1")} />
+                    <Button value={"2"} onClick={() => takeInput("2")} />
+                    <Button value={"3"} onClick={() => takeInput("3")} />
+                    <Button value={"4"} onClick={() => takeInput("4")} />
 
-        <Button value={"1"} onClick={() => takeInput("1")} />
-        <Button value={"2"} onClick={() => takeInput("2")} />
-        <Button value={"3"} onClick={() => takeInput("3")} />
-        <Button value={"-"} onClick={() => takeInput("-")} />
+                    <Button value={"5"} onClick={() => takeInput("5")} />
+                    <Button value={"6"} onClick={() => takeInput("6")} />
+                    <Button value={"7"} onClick={() => takeInput("7")} />
+                    <Button value={"8"} onClick={() => takeInput("8")} />
 
-        <Button value={"0"} onClick={() => takeInput("0")} />
-        <Button value={"."} onClick={() => takeInput(".")} />
-        <Button value={"%"} onClick={() => takeInput("%")} />
-        <Button value={"+"} onClick={() => takeInput("+")} />
+                    <Button value={"9"} onClick={() => takeInput("9")} />
+                    <Button value={"+"} onClick={() => takeInput("+")} />
+                    <Button value={"0"} onClick={() => takeInput("0")} />
+                    <Button value={"-"} onClick={() => takeInput("-")} />
 
-        <Button value={"="} onClick={calculate} />
-        <Button value={"DEL"} onClick={handleDelete} />
-        <Button value={"AC"} onClick={handleClear} />
-        </div>
-    </div>
-    </>
+                    <Button value={"/"} onClick={() => takeInput("/")} />
+                    <Button value={"."} onClick={() => takeInput(".")} />
+                    <Button value={"*"} onClick={() => takeInput("*")} />
+                    <Button value={"%"} onClick={() => takeInput("%")} />
+
+                    <Button value={"="} onClick={calculate} />
+                    <Button value={"DEL"} onClick={handleDelete} />
+                    </div>
+
+                    <Button id="AC" value={"AC"} onClick={handleClear} />
+
+
+            </div>
+        </>
     );
 }
 
